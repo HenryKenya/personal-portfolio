9ac644407a09d313974097025de19d1d55e82c60
henry <dru@Henrys-MacBook-Pro.local>
2020-05-02 21:56:06 +0300

    Add links to about section

--- src/components/about/AboutSection.js
+++ src/components/about/AboutSection.js
@@ -25,10 +25,25 @@ const AboutSection = () => (
         <p>
           The long story? I'm an Electronics engineer turned software engineer
           with close to five years professional experience. Currently, I work as
-          an instructor at <strong>Moringa School </strong>and the Android Lead
-          for <strong>Sklyline Design</strong>. I've also co-founded my own
-          company, <strong>Roometo</strong>, and we help students find
-          alternative accommodation.
+          an instructor at{" "}
+          <strong>
+            <a href="https://moringaschool.com/" target="_blank">
+              Moringa School{" "}
+            </a>
+          </strong>
+          and the Android Lead for{" "}
+          <strong>
+            <a href="https://skylinedesign.co.ke/" target="_blank">
+              Sklyline Design
+            </a>
+          </strong>
+          . I've also co-founded my own company,{" "}
+          <strong>
+            <a href="https://roometo.com/about" target="_blank">
+              Roometo
+            </a>
+          </strong>
+          , and we help students find alternative accommodation.
         </p>
         <p>
           JavaScript, Kotlin, Swift and Java are my primary stack. I have
@@ -50,10 +65,22 @@ const AboutSection = () => (
         </p>
         <p>
           Beyond programming, I also write. I'm a contributor to
-          <strong> Qazini</strong> and <strong>Analytics Vidhya</strong>. When
-          I'm not doing all these I'm probably playing video games, watching an
-          anime or learning something new. At the moment, I'm try to learn sign
-          language and is considering learning German and being a chef.
+          <strong>
+            <a href="https://www.qazini.com/" target="_blank">
+              {" "}
+              Qazini
+            </a>
+          </strong>{" "}
+          and{" "}
+          <strong>
+            <a href="https://www.analyticsvidhya.com/" target="_blank">
+              Analytics Vidhya
+            </a>
+          </strong>
+          . When I'm not doing all these I'm probably playing video games,
+          watching an anime or learning something new. At the moment, I'm try to
+          learn sign language and is considering learning German and being a
+          chef.
         </p>
       </div>
     </div>
