e6e281d1a813740498a1490045a45b6e5ac91ee8
henry <henry.onyango@outlook.com>
2020-05-02 23:29:08 +0300

    Updates

---
+++ precache-manifest.41552696dd5250a8d39c0d4502537e7d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6783b881f1e4b7b73e8c5021ab057e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90dc094c7c7646f0730b",
+    "url": "/static/css/main.972ee06a.chunk.css"
+  },
+  {
+    "revision": "b382e70af2ff84e6df01",
+    "url": "/static/js/2.20ebbbb8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.20ebbbb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90dc094c7c7646f0730b",
+    "url": "/static/js/main.750a9f32.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "502110097364ca165809cd41c9460764",
+    "url": "/static/media/gsea_logo.50211009.png"
+  },
+  {
+    "revision": "701b2c954a0c1a845ea5b61eb2abc52a",
+    "url": "/static/media/hero-bg.701b2c95.jpg"
+  },
+  {
+    "revision": "4070da2b97e513d4d4dbebdf5b9939ff",
+    "url": "/static/media/sahara_logo.4070da2b.png"
+  }
+]);
