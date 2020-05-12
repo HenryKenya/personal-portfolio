7174e05cca1056086a0fed2612d0f7d893f15ba6
henry <henry.onyango@outlook.com>
2020-05-12 21:14:55 +0300

    Configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,12 @@
-// import { compose} from 'redux'
+import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 
-// export default function configureStore(initialState) {
-//     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// }
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant))
+  );
+}
