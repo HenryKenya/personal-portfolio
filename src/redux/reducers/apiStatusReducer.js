f6b1fbc7717b20463782d3b985d29a0a23efffcd
henry <henry.onyango@outlook.com>
2020-05-12 21:47:32 +0300

    Implement reducer for articles

--- src/redux/reducers/apiStatusReducer.js
+++ src/redux/reducers/apiStatusReducer.js
@@ -5,11 +5,17 @@ function actionTypesEndInSuccess(type) {
   return type.substring(type.length - 8) === "_SUCCESS";
 }
 
-export default apiStatusReducer(state = initialState.apiCallsInProgress, action) {
-    if(action.type ==  "BEGIN_API_CALL") {
-        return state + 1
-    } else if(action.type == types.API_CALL_ERROR || actionTypesEndInSuccess(type)) {
-        return state - 1
-    }
-    return state;
+export default function apiStatusReducer(
+  state = initialState.apiCallsInProgress,
+  action
+) {
+  if (action.type === "BEGIN_API_CALL") {
+    return state + 1;
+  } else if (
+    action.type === types.API_CALL_ERROR ||
+    actionTypesEndInSuccess(action.type)
+  ) {
+    return state - 1;
+  }
+  return state;
 }
