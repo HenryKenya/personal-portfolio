b5de751dab81c6789508d01a4369229353f87a44
henry <henry.onyango@outlook.com>
2020-05-12 20:56:09 +0300

    Install redux and dependencies

---
+++ src/redux/configureStore.js
@@ -0,0 +1,5 @@
+// import { compose} from 'redux'
+
+// export default function configureStore(initialState) {
+//     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// }
