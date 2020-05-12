1420558c83516349eded4552540f6156e5c96093
henry <henry.onyango@outlook.com>
2020-05-12 21:25:54 +0300

    Implement action creator for articles and api status

---
+++ src/redux/actions/articlesActions.js
@@ -0,0 +1,23 @@
+import * as types from "../../helpers/constants";
+import * as articlesApi from "../../api/articlesApi";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
+
+export function loadArticlesSuccess(articles) {
+  return { type: types.LOAD_ARTICLES_SUCCESS, articles };
+}
+
+export function loadArticles() {
+  return function (dispatch) {
+    dispatch(beginApiCall());
+
+    return articlesApi
+      .getArticles()
+      .then((articles) => {
+        dispatch(loadArticlesSuccess(articles));
+      })
+      .catch((error) => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
