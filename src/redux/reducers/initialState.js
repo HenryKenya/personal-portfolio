bee8d2f22a5f6d47698babc4788fcd26ededd3d7
henry <henry.onyango@outlook.com>
2020-05-18 22:02:17 +0300

    Implement media reducer

--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,5 +1,5 @@
 export default {
   articles: [],
-  media: {},
+  media: [],
   apiCallsInProgress: 0,
 };
---
+++ src/redux/reducers/mediaReducer.js
@@ -0,0 +1,11 @@
+import * as types from "../../helpers/constants";
+import initialState from "./initialState";
+
+export default function mediaReducer(state = initialState.media, action) {
+  switch (action.type) {
+    case types.LOAD_IMAGES_SUCCESS:
+      return action.media;
+    default:
+      return state;
+  }
+}
